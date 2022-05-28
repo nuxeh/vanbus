5e6c09ea0350322b2f59931395554f60ec9fa99f
Ed Cragg <ed.cragg@eipi.xyz>
2022-05-28 12:38:56 +0100

    Initial implementation of message

---
+++ src/vanbus.h
@@ -0,0 +1,49 @@
+enum VanbusMsgType {
+  Vb_Byte,
+  Vb_Float,
+  Vb_Short,
+  Vb_UShort,
+  Vb_Long,
+  Vb_ULong,
+  Vb_Fixed,
+};
+
+class VanbusFixedPointNum {
+  uint16_t value;
+  uint16_t divisor;
+};
+
+class VanbusMsg {
+  public:
+    VanbusMsg();
+    VanbusMsg(uint8_t pathA, uint8_t pathB, uint8_t field, VanbusMessageType type=Vb_Byte, payload=0);
+
+    void parseFromBytes(uint8_t *bytes, size_t len);
+    int writeBytes(uint8_t *bytes, size_t max);
+
+    void getType();
+    void setType();
+
+    uint8_t getByte();
+    float getFloat();
+    int16_t getShort();
+    uint16_t getUnsignedShort();
+    int32_t getLong();
+    uint32_t getUnsignedLong();
+    VanbusFixedPointNum getFixedPoint();
+
+    void getByte();
+    void getFloat();
+    void getShort();
+    void getUnsignedShort();
+    void getLong();
+    void getUnsignedLong();
+    void getFixedPoint();
+
+  private:
+    uint8_t pathA = 0;
+    uint8_t pathB = 0;
+    uint8_t field = 0;
+    VanbusMessageType type = Vb_Byte;
+    uint32_t payload = 0;
+};
