09c00970bbe16e0f01828dd309ebefcba864ab6d
Ed Cragg <ed.cragg@eipi.xyz>
2022-05-29 15:36:55 +0100

    Long
    Set 0xA2, length: 5
    Wrote to buffer, length: 5
    Parsed from buffer, read bytes: 5
    Read value: 0xA2
    Set 0xA2A2, length: 6
    Wrote to buffer, length: 6
    Parsed from buffer, read bytes: 6
    Read value: 0xA2A2
    Set 0xA1A2A3A4, length: 8
    Wrote to buffer, length: 8
    Parsed from buffer, read bytes: 8
    Read value: 0xA1A2A3A4

--- examples/overview/src/overview.ino
+++ examples/overview/src/overview.ino
@@ -7,7 +7,8 @@ void setup() {
 
   // tests
   read_write_byte();
-  read_write_short();
+  read_write_ushort();
+  read_write_ulong();
 }
 
 void read_write_byte() {
@@ -34,7 +35,7 @@ void read_write_byte() {
   Serial.println(msg2.getByte(), HEX);
 }
 
-void read_write_short() {
+void read_write_ushort() {
   uint8_t buf[8];
 
   VanbusMsg msg;
@@ -56,6 +57,30 @@ void read_write_short() {
   Serial.println(read_bytes);
   Serial.print("Read value: 0x");
   Serial.println(msg2.getUnsignedShort(), HEX);
+}
+
+void read_write_ulong() {
+  uint8_t buf[8];
+
+  VanbusMsg msg;
+  uint32_t foo = 0xA1A2A3A4;
+  msg.set(foo);
+
+  Serial.print("Set 0xA1A2A3A4, length: ");
+  Serial.println(msg.len());
+
+  uint8_t written = msg.writeBytes(buf, 8);
+
+  Serial.print("Wrote to buffer, length: ");
+  Serial.println(written);
+
+  VanbusMsg msg2;
+  int read_bytes = msg2.parseFromBytes(buf, written);
+
+  Serial.print("Parsed from buffer, read bytes: ");
+  Serial.println(read_bytes);
+  Serial.print("Read value: 0x");
+  Serial.println(msg2.getUnsignedLong(), HEX);
 }
 
 void loop() {}
