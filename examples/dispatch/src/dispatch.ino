6faf2aad9502b9be37f7d39c3e8afba177cf3a35
Ed Cragg <ed.cragg@eipi.xyz>
2022-05-29 21:06:47 +0100

    Hmm
    Message: a/b/c
    > Function a/b/c called
    > Function a/b/* called
    > Function a/*/* called
    > Function */b/* called
    > Function * called
    Message: a/b/d
    > Function a/b/d called
    > Function a/b/* called
    > Function a/*/* called
    > Function */b/* called
    > Function * called
    Message: a/b/*
    > Function a/b/* called
    > Function a/*/* called
    > Function */b/* called
    > Function * called
    Message: a/*/*
    > Function a/*/* called
    > Function * called
    Message: */b/*
    > Function */b/* called
    > Function * called

--- examples/dispatch/src/dispatch.ino
+++ examples/dispatch/src/dispatch.ino
@@ -4,23 +4,23 @@
 Vanbus<6> vanbus;
 
 void a_b_c(VanbusMsg *m) {
-  Serial.println(F("> Function A/B/C called"));
+  Serial.println(F("> Function a/b/c called"));
 }
 
 void a_b_d(VanbusMsg *m) {
-  Serial.println(F("> Function A/B/D called"));
+  Serial.println(F("> Function a/b/d called"));
 }
 
 void a_b(VanbusMsg *m) {
-  Serial.println(F("> Function A/B/* called"));
+  Serial.println(F("> Function a/b/* called"));
 }
 
 void a(VanbusMsg *m) {
-  Serial.println(F("> Function A/*/* called"));
+  Serial.println(F("> Function a/*/* called"));
 }
 
 void b(VanbusMsg *m) {
-  Serial.println(F("> Function B/*/* called"));
+  Serial.println(F("> Function */b/* called"));
 }
 
 void all(VanbusMsg *m) {
@@ -39,27 +39,30 @@ void setup() {
 
   uint8_t buf[8];
 
+  Serial.println(F("Message: a/b/c"));
   VanbusMsg msg('a', 'b', 'c');
   msg.writeBytes(buf, 8);
   vanbus.receive(buf, 8);
 
+  Serial.println(F("Message: a/b/d"));
   VanbusMsg msg2('a', 'b', 'd');
   msg2.writeBytes(buf, 8);
   vanbus.receive(buf, 8);
 
-#if 1
+  Serial.println(F("Message: a/b/*"));
   VanbusMsg msg3('a', 'b');
   msg3.writeBytes(buf, 8);
   vanbus.receive(buf, 8);
 
+  Serial.println(F("Message: a/*/*"));
   VanbusMsg msg4('a');
   msg4.writeBytes(buf, 8);
   vanbus.receive(buf, 8);
 
+  Serial.println(F("Message: */b/*"));
   VanbusMsg msg5(0, 'b');
   msg5.writeBytes(buf, 8);
   vanbus.receive(buf, 8);
-#endif
 }
 
 void loop() {}
